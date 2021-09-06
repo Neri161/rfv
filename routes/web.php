c414ba45b016338eb84408f3e0236f05f8ec5229
GON161 <nerigallatmon@gmail.com>
2021-09-06 11:09:21 -0500

    v0.1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('bienvenida');
 });
+
+Route::get('/Bienvenida',[UsuarioController::class,'bienvenida'])->name('bienvenida');
