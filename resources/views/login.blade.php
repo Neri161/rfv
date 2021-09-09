5af13cb2ff7331e78bd21ef1b0cc5f17b8f36adf
Casandra <casandraceballosgarza@gmail.com>
2021-09-09 01:20:36 -0500

    v3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,6 +8,7 @@
     <title>Iniciar Sesion</title>
     <link rel="stylesheet" href="css/login.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="https://kit.fontawesome.com/7c0f4c4dd5.js" crossorigin="anonymous"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!------ Include the above in your HEAD tag ---------->
@@ -16,69 +17,56 @@
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    
 </head>
 <body>
-<div class="container">
+<div class="container box box_login shadow0">
     <div class="row">
         <div class="col-md-5 mx-auto">
             <div id="first">
                 <div class="myform form ">
                     <div class="logo mb-3">
                         <div class="col-md-12 text-center">
-                            <h1>Login</h1>
+                            <h1>Inicio de Sesion</h1>
                         </div>
                     </div>
                     <form action="" method="post" name="login">
-                        {{csrf_field()}}
-                        @if(isset($estatus))
-                            @if($estatus == "success")
-                                <label class="bg-success text-white col-md-12 text-center">{{$mensaje}}</label>
-                            @elseif($estatus == "error")
-                                <label class="bg-danger text-white col-md-12 text-center">{{$mensaje}}</label>
-                            @endif
-                        @endif
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" name="email" class="form-control" id="email"
-                                   aria-describedby="emailHelp" placeholder="Enter email">
+                            <label for="correo">Correo:</label>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping"><i class="fas fa-at"></i></span>
+                                <input type="text" name="correo" id="correo" class="form-control" placeholder="correo" aria-label="correo" aria-describedby="addon-wrapping">
+                              </div>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Password</label>
-                            <input type="password" name="password" id="password" class="form-control"
-                                   aria-describedby="emailHelp" placeholder="Enter Password">
+                            <label for="password">Contraseña:</label>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping"><i class="fas fa-lock"></i></span>
+                                <input type="text" name="password" id="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="password">
+                              </div>
                         </div>
                         <div class="form-group">
-                            <p class="text-center">By signing up you accept our <a href="#">Terms Of Use</a></p>
+                            <a href="" class="recuperar">
+                             ¿Olvidaste tu contraseña?
                         </div>
                         <div class="col-md-12 text-center ">
-                            <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Login</button>
+                            <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Iniciar Sesion</button>
                         </div>
-                        <div class="col-md-12 ">
-                            <div class="login-or">
-                                <hr class="hr-or">
-                                <span class="span-or">or</span>
-                            </div>
-                        </div>
+                        
                         <div class="col-md-12 mb-3">
                             <p class="text-center">
-                                <a href="javascript:void();" class="google btn mybtn"><i class="fa fa-google-plus">
-                                    </i> Signup using Google
-                                </a>
+                                
                             </p>
                         </div>
-                        @if(isset($_GET["r"]))
-                            <input type="hidden" name="url" value="{{$_GET["r"]}}">
-                        @endif
                         <div class="form-group">
-                            <p class="text-center">Don't have account? <a href="{{route('registro')}}" id="signup">Sign
-                                    up here</a></p>
+                            <p class="text-center">¿No tienes cuenta? <a href="{{route('registro')}}" id="signup">Registrate</a></p>
                         </div>
                     </form>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
-
 </body>
 </html>
