00941c6c3df0d3836dbb7f45107229f307f0c751
GON161 <nerigallatmon@gmail.com>
2021-10-05 18:03:07 -0500

    v0.5

---
+++ resources/views/admin/listaUsuarios.blade.php
@@ -0,0 +1,16 @@
+@extends('admin.layout.usuario')
+
+@section('titulo')
+    <title>Inicio</title>
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('contenido')
+@endsection
+
+@section('js')
+
+@endsection
