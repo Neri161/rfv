4a659e07855f3bd0f24fa4f91138e566158ee465
GON161 <nerigallatmon@gmail.com>
2021-10-05 18:00:27 -0500

    v0.5

--- resources/views/admin/layout/usuario.blade.php
+++ resources/views/admin/layout/usuario.blade.php
@@ -30,7 +30,7 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin.inicio')}}">
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-laugh-wink"></i>
             </div>
@@ -83,7 +83,7 @@
                 <div class="bg-white py-2 collapse-inner rounded">
                     <!--<h6 class="collapse-header">Custom Utilities:</h6>-->
                     <a class="collapse-item" href="{{route('admin.editarusuario')}}"><i class="fas fa-user-tie"></i>
-                        Cambiar Datos</a>
+                        Lista Usuarios</a>
 
                 </div>
             </div>
